dfb879856cf210815cf1acf8b72535533c2e2dad
Django Merope <dmerope@gmail.com>
2025-03-19 11:21:50 +0900

    Send Pino logs to STDOUT as well as Sentry
    Previously, production logs were only going to Sentry. It can be helpful
    to have detailed debugging logs available on the STDOUT. In the future,
    we will need to find a way to make this accessible (eg: write to S3 or
    something)

--- src/lib/server/utils/logs/pino.ts
+++ src/lib/server/utils/logs/pino.ts
@@ -1,27 +1,31 @@
-import { default as pino_instance } from 'pino';
+import { default as pino_instance, transport as pino_transport } from 'pino';
 import { LOG_LEVEL } from '$env/static/private';
 import { PUBLIC_SENTRY_DSN } from '$env/static/public';
 import { dev } from '$app/environment';
 
+const productionTransports = pino_transport({
+	targets: [
+		{
+			target: 'pino-sentry-transport',
+			options: {
+				sentry: {
+					dsn: PUBLIC_SENTRY_DSN, // sentry dsn
+					environment: dev ? 'development' : 'production'
+					// additional options for sentry
+				},
+				withLogRecord: true, // default false - send the entire log record to sentry as a context.(FYI if its more then 8Kb Sentry will throw an error)
+				tags: ['level'], // sentry tags to add to the event, uses lodash.get to get the value from the log record
+				context: ['hostname'], // sentry context to add to the event, uses lodash.get to get the value from the log record,
+				minLevel: 40 // which level to send to sentry
+			}
+		},
+		{ target: 'pino/file', options: { destination: 1 }, level: LOG_LEVEL ?? 'debug' } //writes to STDOUT
+	]
+});
+
 const logger = dev
 	? pino_instance({ level: LOG_LEVEL ?? 'debug' })
-	: pino_instance({
-			level: LOG_LEVEL ?? 'debug',
-			transport: {
-				target: 'pino-sentry-transport',
-				options: {
-					sentry: {
-						dsn: PUBLIC_SENTRY_DSN, // sentry dsn
-						environment: dev ? 'development' : 'production'
-						// additional options for sentry
-					},
-					withLogRecord: true, // default false - send the entire log record to sentry as a context.(FYI if its more then 8Kb Sentry will throw an error)
-					tags: ['level'], // sentry tags to add to the event, uses lodash.get to get the value from the log record
-					context: ['hostname'], // sentry context to add to the event, uses lodash.get to get the value from the log record,
-					minLevel: 40 // which level to send to sentry
-				}
-			}
-		});
+	: pino_instance(productionTransports);
 
 export function pino(file: string) {
 	const child = logger.child({ file });
