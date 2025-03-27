78ec5ad9235bc56bc7ba7d2212032e0fc7802f4b
Django Merope <dmerope@gmail.com>
2025-03-27 10:56:32 +0900

    Update tests to check signedInAdmin

--- src/routes/(api)/api/v1/admins/[admin_id]/server.test.ts
+++ src/routes/(api)/api/v1/admins/[admin_id]/server.test.ts
@@ -2,7 +2,7 @@ import { expect, describe, it, vi, beforeEach } from 'vitest';
 import { DELETE } from './+server';
 import { Localization } from '$lib/i18n';
 const t = new Localization('en');
-
+import * as m from '$lib/paraglide/messages';
 const mockQueue = vi.fn();
 
 // Mock the error function from $lib/server
@@ -36,10 +36,21 @@ describe('Admin Deletion API', () => {
 	const mockEvent = {
 		locals: {
 			instance: { id: 1 },
+			admin: { id: 2 },
 			t,
 			queue: mockQueue
 		},
 		params: { admin_id: '1' }
+	};
+
+	const mockEventSelfDeleteError = {
+		locals: {
+			instance: { id: 1 },
+			admin: { id: 2 },
+			t,
+			queue: mockQueue
+		},
+		params: { admin_id: '2' }
 	};
 
 	beforeEach(() => {
@@ -58,6 +69,7 @@ describe('Admin Deletion API', () => {
 			instance_id: 1,
 			admin_id: 1,
 			t: mockEvent.locals.t,
+			currentlySignedInAdminId: 2,
 			queue: mockQueue
 		});
 	});
@@ -91,6 +103,39 @@ describe('Admin Deletion API', () => {
 			error: true,
 			name: 'Error',
 			message: 'Cannot delete last admin',
+			id: '1'
+		});
+	});
+
+	it('stops an admin from deleting themselves', async () => {
+		const { del } = await import('$lib/server/api/core/admins');
+		const testError = new Error(m.wide_male_parrot_enrich());
+		vi.mocked(del).mockRejectedValueOnce(testError);
+
+		// Get the mocked error function
+		const { error } = await import('$lib/server');
+
+		const response = await DELETE(mockEventSelfDeleteError);
+
+		// Check that error was called with the right parameters
+		expect(error).toHaveBeenCalledWith(
+			500,
+			'API:/admins/[admin_id]:DELETE:01',
+			expect.any(String),
+			testError
+		);
+
+		// Check the status code
+		expect(response.status).toBe(500);
+
+		// Parse the response body
+		const responseBody = await response.json();
+
+		// Expect the error structure
+		expect(responseBody).toEqual({
+			error: true,
+			name: 'Error',
+			message: m.wide_male_parrot_enrich(),
 			id: '1'
 		});
 	});
