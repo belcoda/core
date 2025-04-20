a6db735a7cabefde5d42e069d0790a66a2ce1bda
Django Merope <dmerope@gmail.com>
2025-04-20 16:31:22 +0900

    When checking names of event, still return deleted events to ensure name is unique

--- src/lib/server/api/events/events.ts
+++ src/lib/server/api/events/events.ts
@@ -115,7 +115,7 @@ export async function create({
 		let counter = 1;
 		while (true) {
 			const exists =
-				await db.sql`SELECT id FROM events.events WHERE instance_id = ${db.param(instanceId)} AND (name = ${db.param(uniqueName)} OR slug = ${db.param(uniqueSlug)}) AND deleted_at IS NULL`.run(
+				await db.sql`SELECT id FROM events.events WHERE instance_id = ${db.param(instanceId)} AND (name = ${db.param(uniqueName)} OR slug = ${db.param(uniqueSlug)})`.run(
 					txnClient
 				);
 
