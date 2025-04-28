2d4b8965534fa4cb47aefebb8a69291d3ff63967
Django Merope <dmerope@gmail.com>
2025-04-28 20:26:48 +0900

    Update unique index to exclude deleted records

--- db/migrations/20250328035833_unique_person_info.cjs
+++ db/migrations/20250328035833_unique_person_info.cjs
@@ -1,11 +1,13 @@
 exports.up = async function (knex) {
 	await knex.schema.raw(`
     CREATE UNIQUE INDEX unique_instance_phone
-    ON people.people (instance_id, (phone_number->>'phone_number'));
+    ON people.people (instance_id, (phone_number->>'phone_number'))
+    WHERE deleted_at is null;
   `);
 	await knex.schema.raw(`
     CREATE UNIQUE INDEX unique_instance_email
-    ON people.people (instance_id, (email->>'email'));
+    ON people.people (instance_id, (email->>'email'))
+    WHERE deleted_at is null;
   `);
 };
 exports.down = async function (knex) {
