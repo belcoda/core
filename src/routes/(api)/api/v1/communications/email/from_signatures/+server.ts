fb0ed00ab8dd1d8cbcfb42c3530ba12aae4705a3
Django Merope <dmerope@gmail.com>
2025-05-12 11:59:27 +0900

    API routes for email from signatures

---
+++ src/routes/(api)/api/v1/communications/email/from_signatures/+server.ts
@@ -0,0 +1,40 @@
+import { json, error, pino } from '$lib/server';
+const log = pino(import.meta.url);
+import * as api from '$lib/server/api/communications/email/from_signatures';
+import * as m from '$lib/paraglide/messages';
+export async function GET(event) {
+	try {
+		const read = await api.list({
+			instanceId: event.locals.instance.id,
+			url: event.url
+		});
+		return json(read);
+	} catch (err) {
+		log.error(err);
+		return error(
+			500,
+			'API:/api/v1/communications/emails/from_signatures:GET01',
+			m.spry_ago_baboon_cure(),
+			err
+		);
+	}
+}
+
+export async function POST(event) {
+	try {
+		const body = await event.request.json();
+		const created = await api.create({
+			instanceId: event.locals.instance.id,
+			body
+		});
+		return json(created);
+	} catch (err) {
+		log.error(err);
+		return error(
+			500,
+			'API:/api/v1/communications/emails/from_signatures:POST01',
+			m.spry_ago_baboon_cure(),
+			err
+		);
+	}
+}
---
+++ src/routes/(api)/api/v1/communications/email/from_signatures/[from_signature_id]/+server.ts
@@ -0,0 +1,41 @@
+import { json, error, pino } from '$lib/server';
+const log = pino(import.meta.url);
+import * as api from '$lib/server/api/communications/email/from_signatures';
+import * as m from '$lib/paraglide/messages';
+export async function GET(event) {
+	try {
+		const list = await api.read({
+			instanceId: event.locals.instance.id,
+			fromSignatureId: Number(event.params.from_signature_id)
+		});
+		return json(list);
+	} catch (err) {
+		log.error(err);
+		return error(
+			500,
+			'API:/api/v1/communications/emails/from_signatures/[from_signature_id]:GET01',
+			m.spry_ago_baboon_cure(),
+			err
+		);
+	}
+}
+
+export async function PUT(event) {
+	try {
+		const body = await event.request.json();
+		const updated = await api.update({
+			instanceId: event.locals.instance.id,
+			fromSignatureId: Number(event.params.from_signature_id),
+			body
+		});
+		return json(updated);
+	} catch (err) {
+		log.error(err);
+		return error(
+			500,
+			'API:/api/v1/communications/emails/from_signatures/[from_signature_id]:PUT',
+			m.spry_ago_baboon_cure(),
+			err
+		);
+	}
+}
